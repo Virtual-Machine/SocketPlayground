bae4f639721148bdcf623b053c228ea2176d7588
Virtual-Machine <darrelm@tech-asteroid.com>
2016-05-07 14:37:37 -0400

    Correct passphrase issue, add basic route, and improve server logging

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const http = require('http');
 const https = require('https');
 const privateKey  = fs.readFileSync('certs/server.key', 'utf8');
 const certificate = fs.readFileSync('certs/server.crt', 'utf8');
+const phrase = fs.readFileSync('certs/phrase', 'utf8');
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = {key: privateKey, cert: certificate, passphrase: phrase};
 const express = require('express');
 const app = express();
 
 app.use(express.static('public/js/'))
 app.use(express.static('public/css/'))
 
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(80);
-httpsServer.listen(443);
+httpServer.listen(8000);
+httpsServer.listen(8443);
+
+console.log("localhost:8000 accepting http requests")
+console.log("localhost:8443 accepting https requests")
